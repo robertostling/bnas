d20e2fb01454f3863e4fd6a3457751630d4b4e83
Robert Östling <robert.ostling@helsinki.fi>
2016-05-13 22:48:23 +0300

    Clarified documentation for utils module.

--- bnas/utils.py
+++ bnas/utils.py
@@ -3,6 +3,6 @@
 import theano.tensor as T
 
 def expand_to_batch(x, batch_size, dim=-2):
-    """Expand the second-last dimension of `x` to `batch_size`."""
+    """Expand one dimension of `x` to `batch_size`."""
     return T.shape_padaxis(x, dim).repeat(batch_size, axis=dim)
 
