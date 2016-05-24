0fd8aa05e7f4e71e6d20ece4c831b09edf370b6f
Robert Östling <robert.ostling@helsinki.fi>
2016-05-24 15:08:37 +0300

    Added 3D softmax function.

--- bnas/utils.py
+++ bnas/utils.py
@@ -6,3 +6,10 @@ def expand_to_batch(x, batch_size, dim=-2):
     """Expand one dimension of `x` to `batch_size`."""
     return T.shape_padaxis(x, dim).repeat(batch_size, axis=dim)
 
+
+def softmax_3d(x):
+    """Generalization of T.nnet.softmax for 3D tensors"""
+    return T.nnet.softmax(x.reshape(
+        (x.shape[0]*x.shape[1], x.shape[2]))).reshape(
+        (x.shape[0], x.shape[1], x.shape[2]))
+
