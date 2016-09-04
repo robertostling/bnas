d5b0f704a633e7e97067cddc8d90f376d3e59ff6
Robert Östling <robert@ling.su.se>
2016-09-04 22:40:01 +0200

    Added text.TextEncoder class

--- bnas/text.py
+++ bnas/text.py
@@ -1,4 +1,8 @@
-"""Text processing."""
+"""Text processing.
+
+The :class:`TextEncoder` class is the main feature of this module, the helper
+functions were used in earlier examples and should be phased out.
+"""
 
 from collections import Counter
 
@@ -6,7 +10,104 @@ import numpy as np
 import theano
 
 
-def encode_sequences(sequences, max_n_symbols=None,
+class TextEncoder:
+    def __init__(self,
+                 max_vocab=None,
+                 min_count=None,
+                 vocab=None,
+                 sequences=None,
+                 sub_encoder=None,
+                 special=('<S>', '</S>', '<UNK>')):
+        self.sub_encoder = sub_encoder
+        self.special = special
+
+        if vocab is not None:
+            self.vocab = vocab
+        else:
+            if sequences is not None:
+                c = Counter(x for xs in sequences for x in xs)
+                if max_vocab is not None:
+                    self.vocab = special + tuple(
+                            s for s,_ in c.most_common(max_vocab))
+                elif min_count is not None:
+                    self.vocab = special + tuple(
+                            s for s,n in c.items() if n >= min_count)
+                else:
+                    self.vocab = special + tuple(c.keys())
+
+        self.index = {s:i for i,s in enumerate(self.vocab)}
+
+    def __getitem__(self, x):
+        return self.index.get(x, self.index.get('<UNK>'))
+
+    def __len__(self):
+        return len(self.vocab)
+
+    def encode_sequence(self, sequence, length=None, unknowns=None):
+        start = (self.index['<S>'],) if '<S>' in self.index else ()
+        stop = (self.index['</S>'],) if '</S>' in self.index else ()
+        unk = self.index.get('<UNK>')
+        def encode_item(x):
+            idx = self.index.get(x)
+            if idx is None:
+                if unknowns is None:
+                    return unk
+                else:
+                    unknowns.append(x)
+                    return -len(unknowns)
+            else:
+                return idx
+        encoded = tuple(idx for idx in list(map(encode_item, sequence))
+                        if idx is not None)
+        if length is None or len(encoded)+len(start)+len(stop) <= length:
+            return start + encoded + stop
+        else:
+            return start + encoded[:length-(len(start)+len(stop))] + stop
+
+    def pad_sequences(self, sequences,
+                      length=None, pad_right=True, dtype=np.int32):
+        if not sequences:
+            m = np.zeros((0 if length is None else length, 0), dtype=dtype)
+            mask = np.zeros_like(m, dtype=np.bool)
+            return m, mask
+        unknowns = None if self.sub_encoder is None else []
+        encoded_sequences = [self.encode_sequence(sequence, length, unknowns)
+                             for sequence in sequences]
+        if length is None:
+            length = max(map(len, encoded_sequences))
+        else:
+            assert max(map(len, encoded_sequences)) == length
+
+        m = np.zeros((length, len(sequences)), dtype=dtype)
+        mask = np.zeros_like(m, dtype=np.bool)
+
+        for i,encoded in enumerate(encoded_sequences):
+            if pad_right:
+                m[:len(encoded),i] = encoded
+                mask[:len(encoded),i] = 1
+            else:
+                m[-len(encoded):,i] = encoded
+                mask[-len(encoded):,i] = 1
+
+        if unknowns is None:
+            return m, mask
+        else:
+            char, char_mask = self.sub_encoder.pad_sequences(unknowns)
+            return m, mask, char, char_mask
+
+    def decode_padded(self, m, mask, char=None, char_mask=None):
+        if char is not None:
+            unknowns = list(map(
+                ''.join, self.sub_encoder.decode_padded(char, char_mask)))
+        start = self.index.get('<S>')
+        stop = self.index.get('</S>')
+        return [[unknowns[-x-1] if x < 0 else self.vocab[x]
+                 for x,b in zip(row,row_mask)
+                 if bool(b) and x not in (start, stop)]
+                for row,row_mask in zip(m.T,mask.T)]
+
+
+def encode_sequences(sequences, max_n_symbols=None, hybrid=False,
                      special=('<S>', '</S>', '<UNK>'), dtype=np.int64):
     """Encode sequences as numpy arrays of integers.
 
@@ -18,6 +119,9 @@ def encode_sequences(sequences, max_n_symbols=None,
         If given, the total number of symbols (including special symbols) is
         limited to this number. The most common symbols in the given sequences
         are used.
+    hydrid : bool
+        If False, encode all words except the max_n_symbols most common as
+        <UNK>, otherwise ...
     special : tuple of str
         List of special symbols to include.
     dtype : numpy dtype
@@ -38,12 +142,11 @@ def encode_sequences(sequences, max_n_symbols=None,
 
     def create_vocab():
         counts = Counter(sym for seq in sequences for sym in seq)
-        if max_n_symbols is None:
-            vocab = special + tuple(sorted(counts.keys()))
-        else:
-            vocab = special + tuple(sorted(
-                    sym for sym, _ in
-                        counts.most_common(max_n_symbols-len(special))))
+        n_symbols = None if max_n_symbols is None \
+                    else max_n_symbols - len(special)
+        vocab = special + tuple(sorted(
+                sym for sym, _ in
+                    counts.most_common(n_symbols)))
         return vocab
 
     vocab = create_vocab()
